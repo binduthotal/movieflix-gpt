c88f02df730fe328c99de7dc5c642ac277af3ee5
binduthotal <bindus170@gmail.com>
2024-08-12 21:15:50 +0530

    final commit

--- src/components/HomePage/SecondaryContainer/MovieList.js
+++ src/components/HomePage/SecondaryContainer/MovieList.js
@@ -3,19 +3,22 @@ import MovieCard from "./MovieCard";
 
 const MovieList = ({ title, movieList }) => {
     return (
-        <div className="px-5">
-            <h1 className="text-white font-semibold text-2xl py-6">{title}</h1>
-            <div
-                className={`flex ${movieList?.length > 6 ? "overflow-x-scroll" : "overflow-x-hidden"
-                    } `}
-            >
-                <div className="flex ">
-                    {movieList?.map((movie) => (
-                        <MovieCard key={movie.id} poster={movie.poster_path} />
-                    ))}
-                </div>
-            </div>
+      <div className="px-5">
+        <h1 className="text-white font-semibold text-2xl py-6">{title}</h1>
+        <div
+          className={`flex ${
+            movieList?.length > 6
+              ? "overflow-x-scroll scrollbar-hide"
+              : "overflow-x-hidden"
+          } `}
+        >
+          <div className="flex ">
+            {movieList?.map((movie) => (
+              <MovieCard key={movie.id} poster={movie.poster_path} />
+            ))}
+          </div>
         </div>
+      </div>
     );
 };
 
